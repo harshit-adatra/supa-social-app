02fc0162ba7dae587746f97a50a303ff1bb8d471
Harshit Kiran <harshit.kiran@geekyants.com>
2026-04-17 16:45:30 +0530

    feat: added home page

---
+++ app/home.tsx
@@ -0,0 +1,9 @@
+import ScreenWrapper from "@/components/ScreenWrapper";
+import { Text } from "react-native";
+export default function Home() {
+  return (
+    <ScreenWrapper>
+      <Text>Home</Text>
+    </ScreenWrapper>
+  );
+}
